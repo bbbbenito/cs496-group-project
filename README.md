c24dcf80901de0ceef607810f2eb523bfaf714f4
Nils Olsson <nilso@enosis.net>
2019-04-29 19:46:10 -0700

    Update

---
+++ README.md
@@ -0,0 +1,25 @@
+# 2019 Spring, CS-496 group project
+
+## Setup
+
+- Clone or download this repository. It should look like:
+```
+Documents
+└── group-project
+    ├── .gitignore
+    ├── README.md
+    └── project.py
+```
+- Download the [dataset][dataset] and extract it to "SpotifyFeatures.csv"
+in the project directory, so it should look like:
+```
+Documents
+└── group-project
+    ├── .gitignore
+    ├── README.md
+    ├── project.py
+    └── SpotifyFeatures.csv
+```
+- Try running it with `python3 project.py`
+
+[dataset]: https://www.kaggle.com/zaheenhamidani/ultimate-spotify-tracks-db/
--- project.py
+++ project.py
@@ -5,33 +5,21 @@ import pandas as pd
 from matplotlib import pyplot as plt
 from copy import copy
 
-
 pwd = os.getcwd()
-data_dir = './spotify-audio-features'
-data_path_nov2018 = data_dir+'/SpotifyAudioFeaturesNov2018.csv'
-data_path_apr2019 = data_dir+'/SpotifyAudioFeaturesApril2019.csv'
-
+dataset_url = 'https://www.kaggle.com/zaheenhamidani/ultimate-spotify-tracks-db'
+dataset_path = 'SpotifyFeatures.csv'
 
 # Load data sets
 try:
-    try:
-        with open(data_path_nov2018) as f:
-            data_nov2018 = pd.read_csv(f)
-    except FileNotFoundError as e:
-        print('Unable to locate November 2018 data.')
-        raise e
-    try:
-        with open(data_path_apr2019) as f:
-            data_apr2018 = pd.read_csv(f)
-    except FileNotFoundError as e:
-        print('Unable to locate April 2019 data.')
-        raise e
-except:
+    with open(dataset_path) as f:
+        data = pd.read_csv(f)
+except FileNotFoundError as e:
     print(f'''\
-Download the datasets from:
-<https://www.kaggle.com/tomigelo/spotify-audio-features/downloads/spotify-audio-features.zip/3>
-
-And extract to "{pwd}/spotify-audio-features"''')
+Unable to locate dataset at "{dataset_path}".
+Download it at:
+"{dataset_url}"
+And extract to:
+"{pwd}/{dataset_path}"''')
 
 
 # Do stuff...
